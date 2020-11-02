5cf15e9b56b580521a5dbb1ca493105b49c51a1a
likun <996240986@qq.com>
2020-11-02 14:21:42 +0800

    常见链表操作

--- go/02_linkedlist/single.go
+++ go/02_linkedlist/single.go
@@ -4,41 +4,49 @@ import "fmt"
 
 /*
 单链表操作
+链表都有哪些操作？
+1.创建一个链表；
+2.插入数据：在某个节点前，在某个节点后，在链头插入，在链尾插入；
+3.删除数据；
+4.查找数据；
+5.打印链表；
+6.单链表反转
+7.判断链表是否有环；
 */
-type Node struct {
-	next  *Node
+type LinkedNode struct {
+	next  *LinkedNode
 	value interface{}
 }
 
 type LinkedList struct {
-	head   *Node
+	head   *LinkedNode
 	length uint
 }
 
-func NewNode(v interface{}) *Node {
-	return &Node{nil, v}
+func NewNode(v interface{}) *LinkedNode {
+	return &LinkedNode{nil, v}
 }
 
-func (this *Node) GetNext() *Node {
-	return this.next
+func (ln *LinkedNode) GetNext() *LinkedNode {
+	return ln.next
 }
 
-func (this *Node) GetValue() interface{} {
-	return this.value
+func (ln *LinkedNode) GetValue() interface{} {
+	return ln.value
 }
 
-// 创建链表
+//NewLinkedList 创建链表
 func NewLinkedList() *LinkedList {
 	return &LinkedList{NewNode(0), 0}
 }
 
-// 在某个节点前面插入节点
-func (this *LinkedList) InsertBefore(p *Node, v interface{}) bool {
-	if p == nil || p == this.head {
+// InsertBefore 在某个节点前面插入节点
+func (ll *LinkedList) InsertBefore(p *LinkedNode, v interface{}) bool {
+	if p == nil || p == ll.head {
 		return false
 	}
-	cur := this.head.next
-	pre := this.head
+	cur := ll.head.next
+	pre := ll.head
 	for cur != nil {
 		if cur == p {
 			break
@@ -52,12 +60,12 @@ func (this *LinkedList) InsertBefore(p *Node, v interface{}) bool {
 	newNode := NewNode(v)
 	pre.next = newNode
 	newNode.next = cur
-	this.length++
+	ll.length++
 	return true
 }
 
-// 在某个节点后面插入节点
-func (this *LinkedList) InsertAfter(p *Node, v interface{}) bool {
+// InsertAfter 在某个节点后面插入节点
+func (ll *LinkedList) InsertAfter(p *LinkedNode, v interface{}) bool {
 	if p == nil {
 		return false
 	}
@@ -65,30 +73,30 @@ func (this *LinkedList) InsertAfter(p *Node, v interface{}) bool {
 	oldNext := p.next
 	newNode.next = oldNext
 	p.next = newNode
-	this.length++
+	ll.length++
 	return true
 }
 
-// 在链表头部插入节点
-func (this *LinkedList) InsertToHead(v interface{}) bool {
-	return this.InsertAfter(this.head, v)
+// InsertToHead 在链表头部插入节点
+func (ll *LinkedList) InsertToHead(v interface{}) bool {
+	return ll.InsertAfter(ll.head, v)
 }
 
-// 在链表尾部插入节点
-func (this *LinkedList) InsertToTail(v interface{}) bool {
-	cur := this.head
-	for cur != nil {
+// InsertToTail 在链表尾部插入节点
+func (ll *LinkedList) InsertToTail(v interface{}) bool {
+	cur := ll.head
+	for cur.next != nil {
 		cur = cur.next
 	}
-	return this.InsertAfter(cur, v)
+	return ll.InsertAfter(cur, v)
 }
 
-// 通过索引查找节点
-func (this *LinkedList) FindByIndex(index uint) *Node {
-	if index > this.length {
+// FindByIndex 通过索引查找节点
+func (ll *LinkedList) FindByIndex(index uint) *LinkedNode {
+	if index > ll.length {
 		return nil
 	}
-	cur := this.head.next
+	cur := ll.head.next
 	var i uint = 0
 	for ; i < index; i++ {
 		cur = cur.next
@@ -96,13 +104,13 @@ func (this *LinkedList) FindByIndex(index uint) *Node {
 	return cur
 }
 
-// 删除传入的节点
-func (this *LinkedList) Delete(p *Node) bool {
+// Delete 删除传入的节点
+func (ll *LinkedList) Delete(p *LinkedNode) bool {
 	if p == nil {
 		return false
 	}
-	cur := this.head.next
-	pre := this.head
+	cur := ll.head.next
+	pre := ll.head
 	for nil != cur {
 		if p == cur {
 			break
@@ -114,13 +122,13 @@ func (this *LinkedList) Delete(p *Node) bool {
 		return false
 	}
 	pre.next = cur.next
-	this.length--
+	ll.length--
 	return true
 }
 
-// 打印链表
-func (this *LinkedList) Print() {
-	cur := this.head.next
+// Print 打印链表
+func (ll *LinkedList) Print() {
+	cur := ll.head.next
 	format := ""
 	for cur != nil {
 		format += fmt.Sprintf("%+v", cur.GetValue())
@@ -130,4 +138,107 @@ func (this *LinkedList) Print() {
 		}
 	}
 	fmt.Println(format)
+}
+
+// Reverse 链表反转 时间复杂度：O(N)
+func (ll *LinkedList) Reverse() {
+	if ll.head == nil || ll.head.next == nil || ll.head.next.next == nil {
+		return
+	}
+	var pre *LinkedNode = nil
+	cur := ll.head.next
+	for cur != nil {
+		tmp := cur.next
+		cur.next = pre
+		pre = cur
+		cur = tmp
+	}
+	ll.head.next = pre
+}
+
+// HasCycle 判断是否有环
+// 思路：快慢指针，慢指针每次走一步，快指针每次走两步，如果存在环的话，两个指针必定会相遇
+func (ll *LinkedList) HasCycle() bool {
+	if ll.head != nil {
+		slow := ll.head
+		fast := ll.head
+		for fast != nil && fast.next != nil {
+			slow = slow.next
+			fast = fast.next.next
+			if slow == fast {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// FindMiddleNode 获取中间节点
+// 思路：使用快慢指针
+func (ll *LinkedList) FindMiddleNode() *LinkedNode {
+	if ll.head == nil || ll.head.next == nil {
+		return nil
+	}
+	if nil == ll.head.next.next {
+		return ll.head.next
+	}
+	slow, fast := ll.head, ll.head
+	for nil != fast && nil != fast.next {
+		slow, fast = slow.next, fast.next.next
+	}
+	return slow
+}
+
+// MergeSortedList 合并两个有序的链表
+func MergeSortedList(l1, l2 *LinkedList) *LinkedList {
+	if nil == l1 || nil == l1.head || nil == l1.head.next {
+		return l2
+	}
+	if nil == l2 || nil == l2.head || nil == l2.head.next {
+		return l1
+	}
+	l := NewLinkedList()
+	cur := l.head
+	cur1 := l1.head.next
+	cur2 := l2.head.next
+	for nil != cur1 && nil != cur2 {
+		if cur1.value.(int) > cur2.value.(int) {
+			cur.next = cur2
+			cur2 = cur2.next
+		} else {
+			cur.next = cur1
+			cur1 = cur1.next
+		}
+		cur = cur.next
+	}
+	if nil != cur1 {
+		cur.next = cur1
+	} else if nil != cur2 {
+		cur.next = cur2
+	}
+
+	return l
+}
+
+// DeleteBottomN 删除倒数第n个节点
+func (ll *LinkedList) DeleteBottomN(n int) {
+	if n <= 0 || nil == ll.head || nil == ll.head.next {
+		return
+	}
+
+	fast := ll.head
+	for i := 1; i <= n && fast != nil; i++ {
+		fast = fast.next
+	}
+
+	if nil == fast {
+		return
+	}
+
+	slow := ll.head
+	for nil != fast.next {
+		slow = slow.next
+		fast = fast.next
+	}
+	slow.next = slow.next.next
 }
--- go/02_linkedlist/single_test.go
+++ go/02_linkedlist/single_test.go
@@ -1,1 +1,99 @@
 package _2_linkedlist
+
+import "testing"
+
+func TestInsertTail(t *testing.T) {
+	count := 10
+	ll := NewLinkedList()
+	for i := 0; i < count; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+}
+
+func TestInsertHead(t *testing.T) {
+	count := 10
+	ll := NewLinkedList()
+	for i := 0; i < count; i++ {
+		ll.InsertToHead(i)
+	}
+	ll.Print()
+}
+
+func TestFindByIndex(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ln := ll.FindByIndex(uint(3))
+	ll.Print()
+	t.Log(ln.GetValue())
+}
+
+func TestDelete(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ln := ll.FindByIndex(uint(3))
+	ll.Print()
+	ll.Delete(ln)
+	ll.Print()
+}
+
+func TestReverse(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+	ll.Reverse()
+	ll.Print()
+}
+
+func TestHasCycle(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 15; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+	ln := ll.FindByIndex(uint(13))
+	t.Log(ll.HasCycle())
+	ln.next = ll.head.next
+	t.Log(ll.HasCycle())
+}
+
+func TestFindMiddleNode(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+	mid := ll.FindMiddleNode()
+	t.Log(mid.GetValue())
+}
+
+func TestMerge(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+	ll1 := NewLinkedList()
+	for i := 5; i < 15; i++ {
+		ll1.InsertToTail(i)
+	}
+	ll1.Print()
+	res := MergeSortedList(ll, ll1)
+	res.Print()
+}
+
+func TestDeleteBottomN(t *testing.T) {
+	ll := NewLinkedList()
+	for i := 0; i < 10; i++ {
+		ll.InsertToTail(i)
+	}
+	ll.Print()
+	ll.DeleteBottomN(4)
+	ll.Print()
+}
