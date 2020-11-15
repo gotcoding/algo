cd3f9d7d73d10f5ee86aeafea5343e7a9eb0cb26
Terry108 <996240986@qq.com>
2020-11-15 11:54:16 +0800

    shell

--- go/50_sort/shell.go
+++ go/50_sort/shell.go
@@ -1,1 +1,22 @@
 package sort
+
+// 希尔排序
+func shellSort(d []int) []int {
+	for step := len(d) / 2; step > 0; step = step / 2 {
+		for i := step; i < len(d); i++ {
+			tmp := d[i]
+			for j := i - step; j >= 0; j -= step {
+				if tmp < d[j] {
+					d[j+step] = d[j]
+					d[j] = tmp
+					tmp = d[j]
+				} else {
+					break
+				}
+			}
+			// fmt.Printf("%d", step)
+			// fmt.Println(d)
+		}
+	}
+	return d
+}
--- go/50_sort/shell_test.go
+++ go/50_sort/shell_test.go
@@ -1,1 +1,11 @@
 package sort
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestShellSort(t *testing.T) {
+	d := []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+	fmt.Println(shellSort(d))
+}
