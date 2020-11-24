53b731efb9111fc3e97215f5c0b11ac05dbddd5a
likun <996240986@qq.com>
2020-11-24 11:54:22 +0800

    merge array

--- go/01_array/mergearray.go
+++ go/01_array/mergearray.go
@@ -1,29 +1,47 @@
 package arraydev
 
-// 练习1: 合并两个有序数组
-func mergeArray(arr1, arr2 []int) []int {
-	if arr1 == nil {
+// MergeSortedArrays 合并多个有序数组
+// 思路：使用归并的方式来合并
+func MergeSortedArrays(arrs [][]int) []int {
+	return merge(arrs, 0, len(arrs)-1)
+}
+
+// 使用归并的方式来合并
+func merge(arrs [][]int, left, right int) []int {
+	if left == right {
+		return arrs[left]
+	}
+	if left > right {
+		return nil
+	}
+	mid := left + (right-left)>>2
+	return MergeTwoArray(merge(arrs, left, mid), merge(arrs, mid+1, right))
+}
+
+// MergeTwoArray 合并两个有序数组
+func MergeTwoArray(arr1, arr2 []int) []int {
+	if len(arr1) == 0 {
 		return arr2
 	}
-	if arr2 == nil {
+	if len(arr2) == 0 {
 		return arr1
 	}
-	arr := make([]int, 0)
+	res := make([]int, 0)
 	i, j := 0, 0
 	for i < len(arr1) && j < len(arr2) {
-		if arr1[i] < arr2[j] {
-			arr = append(arr, arr1[i])
-			i++
-		} else {
-			arr = append(arr, arr2[j])
+		if arr1[i] > arr2[j] {
+			res = append(res, arr2[j])
 			j++
+		} else {
+			res = append(res, arr1[i])
+			i++
 		}
 	}
-	if i == len(arr1)-1 {
-		arr = append(arr, arr2[j:]...)
+	if i == len(arr1) {
+		res = append(res, arr2[j:]...)
 	}
-	if j == len(arr2)-1 {
-		arr = append(arr, arr1[i:]...)
+	if j == len(arr2) {
+		res = append(res, arr1[i:]...)
 	}
-	return arr
+	return res
 }
