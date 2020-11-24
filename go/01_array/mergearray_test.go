92403ad6a858560b67462b86cc60e75f83693569
likun <996240986@qq.com>
2020-11-24 11:54:04 +0800

    merge array

--- go/01_array/mergearray_test.go
+++ go/01_array/mergearray_test.go
@@ -1,27 +1,55 @@
 package arraydev
 
 import (
-	"fmt"
-	"math/rand"
 	"testing"
 )
 
-func TestMergeArray(t *testing.T) {
-	arr1 := getSortedRandomArray(20)
-	arr2 := getSortedRandomArray(10)
-	fmt.Println(arr1)
-	fmt.Println(arr2)
-	arr := mergeArray(arr1, arr2)
-	fmt.Println(arr)
-}
-
-func getSortedRandomArray(length int) []int {
-	arr := make([]int, length)
-	pre := 0
-	for i := 0; i < length; i++ {
-		tmp := rand.Intn(100)
-		arr[i] = pre + tmp
-		pre = tmp + pre
+func TestMergeSortedArray(t *testing.T) {
+	testCases := []struct {
+		desc string
+		args [][]int
+		want []int
+	}{
+		{
+			desc: "多数组合并-空",
+			args: [][]int{},
+			want: []int{},
+		},
+		{
+			desc: "多数组合并-1个",
+			args: [][]int{
+				{1, 4, 6, 8},
+				{},
+			},
+			want: []int{1, 4, 6, 8},
+		}, {
+			desc: "多数组合并-2个",
+			args: [][]int{
+				{1, 4, 6, 8},
+				{3, 5, 9},
+			},
+			want: []int{1, 3, 4, 5, 6, 8, 9},
+		}, {
+			desc: "多数组合并-多个",
+			args: [][]int{
+				{1, 4, 6, 8},
+				{3, 5, 9},
+				{2, 5, 10},
+			},
+			want: []int{1, 2, 3, 4, 5, 5, 6, 8, 9, 10},
+		},
 	}
-	return arr
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			res := MergeSortedArrays(tC.args)
+			if len(res) != len(tC.want) {
+				t.Error("length not equal")
+			}
+			for i := 0; i < len(res); i++ {
+				if res[i] != tC.want[i] {
+					t.Errorf("want: %v , get: %v", tC.want, res)
+				}
+			}
+		})
+	}
 }
